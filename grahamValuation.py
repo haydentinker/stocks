d1740bd99efd3f7682f16aa80961071c7ac968d9
haydentinker <haydentinker613@gmail.com>
2023-09-04 09:53:28 -0700

    Updating gui and creating exe

--- grahamValuation.py
+++ grahamValuation.py
@@ -5,10 +5,7 @@ from getCurrentYield import getCurrentYield
 def grahamValuation(ticker):
     company = yf.Ticker(ticker)
     eps= float(company.info['trailingEps'])
-    print(f"EPS:{eps}")
     growthRate=get5YearGrowthRate(ticker)
-    print(f"Growth Rate:{growthRate} ")
     currentYield=getCurrentYield()
-    print(f"Current Yield: {currentYield}")
     result=(eps*(8.5+2*growthRate)*(4.4/currentYield))
     return(result)
--- main.py
+++ main.py
@@ -4,56 +4,66 @@ from grahamValuation import grahamValuation
 
 def main():
     root = tk.Tk()
-    root.title("Graham Valuation")
+    root.title("Graham Valuation Calculator")
     root.geometry("500x500")
 
-    # Create a StringVar to store the ticker symbol entered by the user
+    title_label=tk.Label(root,text='Graham Valuation Calculator',font='Calibri 24 bold')
+    title_label.pack()
+    
+    input_frame=tk.Frame(root)
+    # Setting up frame for ticker input 
     ticker = tk.StringVar()
+    ticker.set("AAPL")
+    ticker_frame=tk.Frame(input_frame)
+    tickerLabel = tk.Label(ticker_frame, text="Ticker",font='Calibri 13')
+    tickerLabel.pack()
+    tickerEntry = tk.Entry(ticker_frame, textvariable=ticker)
+    tickerEntry.pack()
+    ticker_frame.pack(pady=10)
+    
+    #Setting up frame for margin of safety input
+    marginSafety_frame=tk.Frame(input_frame)
+    marginOfSafety_val = tk.DoubleVar()
+    marginOfSafety_val.set(0.0)  
+    marginLabel = tk.Label(marginSafety_frame, text="Margin of Safety",font='Calibri 13')
+    marginLabel.pack()
+    marginEntry = tk.Entry(marginSafety_frame, textvariable=marginOfSafety_val)
+    marginEntry.pack()
+    marginSafety_frame.pack(pady=10)
 
-    tickerLabel = tk.Label(root, text="Ticker")
-    tickerLabel.pack(side=tk.TOP, anchor=tk.NW)
+    #Result frame for calculations
+    result_frame=tk.Frame(root)
+    submitButton = tk.Button(input_frame, text="Submit", command=lambda: calculateAndPrint(result_frame, ticker.get(), marginOfSafety_val.get()),font='Calibri 13 bold')
+    submitButton.pack(pady=10)
+    input_frame.pack(pady=10)
+    result_frame.pack(pady=10)
 
-    tickerEntry = tk.Entry(root, textvariable=ticker)
-    tickerEntry.pack(side=tk.TOP, anchor=tk.NW)
-
-    # Create a margin of safety entry field
-    marginOfSafety = tk.DoubleVar()
-    marginOfSafety.set(0.0)  # Set the initial value to 0.0
-
-    marginLabel = tk.Label(root, text="Margin of Safety")
-    marginLabel.pack(side=tk.TOP, anchor=tk.NW)
-
-    marginEntry = tk.Entry(root, textvariable=marginOfSafety)
-    marginEntry.pack(side=tk.TOP, anchor=tk.NW)
-    submitButton = tk.Button(root, text="Submit", command=lambda: calculateAndPrint(root, ticker.get(), marginOfSafety.get()))
-    submitButton.pack(side=tk.TOP, anchor=tk.NW)
-    result_frame = tk.Frame(root)
-    result_frame.pack(side=tk.TOP, anchor=tk.NW)
     root.mainloop()
 
 def calculateAndPrint(root, ticker, marginOfSafety):
     for widget in root.winfo_children():
-        if widget.winfo_parent() == root:
-            widget.destroy()
+        widget.destroy()
     company = yf.Ticker(ticker)
     currentPrice = company.info['currentPrice']
 
-    intrinsicValue = grahamValuation(ticker)
+    intrinsicValue = round(grahamValuation(ticker),2)
     difference = intrinsicValue / currentPrice
     differencePercentage = round((difference * 100), 2)
+    nameLabel = tk.Label(root, text=f"Name: {company.info['longName']}",font='Calibri 18')
+    nameLabel.pack(side=tk.TOP, anchor=tk.NW)
 
-    resultLabel = tk.Label(root, text=f"Current Price: {currentPrice}")
-    resultLabel.pack(side=tk.TOP, anchor=tk.NW)
+    currentPrice_Label = tk.Label(root, text=f"Current Price: {currentPrice}",font='Calibri 18')
+    currentPrice_Label.pack(side=tk.TOP, anchor=tk.NW)
 
-    resultLabel = tk.Label(root, text=f"Intrinsic Value: {intrinsicValue}")
-    resultLabel.pack(side=tk.TOP, anchor=tk.NW)
+    intrinsicValue_Label = tk.Label(root, text=f"Intrinsic Value: {intrinsicValue}",font='Calibri 18')
+    intrinsicValue_Label.pack(side=tk.TOP, anchor=tk.NW)
 
-    resultLabel = tk.Label(root, text=f"Difference: {differencePercentage}%")
-    resultLabel.pack(side=tk.TOP, anchor=tk.NW)
+    differencePercent_Label = tk.Label(root, text=f"Difference: {differencePercentage}%",font='Calibri 18')
+    differencePercent_Label.pack(side=tk.TOP, anchor=tk.NW)
 
     acceptableBuyPrice = intrinsicValue * marginOfSafety
-    resultLabel = tk.Label(root, text=f'Acceptable Buy Price: {acceptableBuyPrice}')
-    resultLabel.pack(side=tk.TOP, anchor=tk.NW)
+    acceptableBuyPrice_Label = tk.Label(root, text=f'Acceptable Buy Price: {acceptableBuyPrice}',font='Calibri 18')
+    acceptableBuyPrice_Label.pack(side=tk.TOP, anchor=tk.NW)
 
 if __name__ == "__main__":
     main()
